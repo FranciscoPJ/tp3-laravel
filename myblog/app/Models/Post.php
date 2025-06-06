f277ddef3846b28f723053f04c730361845b451f
Francisco Pandolfi Jimenez <francisco.pandolfi@est.fi.uncoma.edu.ar>
2025-06-06 15:17:46 -0300

    Agregue modelos y migraciones

---
+++ myblog/app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'poster', 'habilitated', 'content', 'id_user', 'id_category'];
+
+    public function user()
+    {
+        // cada post pertenece a un solo usuario, por eso belongsTo()
+        return $this->belongsTo(User::class);
+    }
+}
